db4b5607bf2e9fe26b22ed0166fd1449853c142d
fjebaker <fergusbkr@gmail.com>
2023-04-03 21:14:31 +0100

    feat: HDU types and typed column reading
    Added a few more reading methods, and abstracted the FITSFile type
    through FITS, which can read HDUs and returns a HDU union type.

--- src/main.zig
+++ src/main.zig
@@ -2,19 +2,60 @@ const std = @import("std");
 const c = @import("c.zig");
 const fits = @import("fits.zig");
 
+const FITS = fits.FITS;
+
 const testing = std.testing;
 
 test "read-header" {
-    const filename = "/data/astro/xmm/Mrk-335/data/1112_0306870101_SCX00000ATS.FIT";
-    var f = try fits.FITSFile.open(filename);
-    defer f.close();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var alloc = gpa.allocator();
 
-    f.readInfo();
-    var i: u32 = 1;
-    while (i <= f.nkeys) : (i += 1) {
-        std.debug.print(
-            "{s}\n",
-            .{f.readRecord(i)},
-        );
+    const filename = "/Users/lx21966/Developer/relline/rel_table.fits";
+    var f = try FITS.initFromFile(filename);
+    defer f.deinit();
+
+    std.debug.print("Num HDUs: {d}\n", .{f.num_hdus});
+    // var hdutypes = try f.getAllHDUTypes(alloc);
+    // defer alloc.free(hdutypes);
+
+    // for (hdutypes) |t| {
+    //     std.debug.print("{}\n", .{t});
+    // }
+
+    const hdu = try f.getHDU(2);
+    var headers = try hdu.readHeader(alloc);
+    defer alloc.free(headers);
+
+    for (headers) |h| {
+        std.debug.print("{s}\n", .{h});
     }
+
+    const cols = try hdu.BinaryTable.getNumColumns();
+    const rows = try hdu.BinaryTable.getNumRows();
+
+    std.debug.print("Dimensions {d} x {d}\n", .{ rows, cols });
+
+    var data = try hdu.BinaryTable.readColumnTyped(f32, 1, alloc);
+    defer alloc.free(data);
+
+    std.debug.print("{any}\n", .{data});
+
+    std.debug.print("\n", .{});
+
+    // var infos = try f.getAllInfo(alloc);
+    // defer alloc.free(infos);
+
+    // for (infos) |info| {
+    //     std.debug.print("{s}\n", .{info});
+    // }
+
+    // try f.readInfo();
+    // var i: u32 = 1;
+    // while (i <= f.nkeys) : (i += 1) {
+    //     std.debug.print(
+    //         "{s}\n",
+    //         .{f.readRecord(i)},
+    //     );
+    // }
 }
