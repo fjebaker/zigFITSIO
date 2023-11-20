d8780dbd25f6dea711c2983831a511ac7d75b0d5
fjebaker <fergusbkr@gmail.com>
2023-11-20 18:34:49 +0000

    feat: install all header files in artifact

--- build.zig
+++ build.zig
@@ -4,6 +4,43 @@ fn _root() []const u8 {
     return (std.fs.path.dirname(@src().file) orelse ".");
 }
 
+const CFITSIO_SOURCES = [_][]const u8{
+    "buffers.c",     "cfileio.c",        "checksum.c",         "drvrfile.c",  "drvrmem.c",
+    "drvrnet.c",     "drvrsmem.c",       "editcol.c",          "edithdu.c",   "eval_l.c",
+    "eval_y.c",      "eval_f.c",         "fitscore.c",         "getcol.c",    "getcolb.c",
+    "getcold.c",     "getcole.c",        "getcoli.c",          "getcolj.c",   "getcolk.c",
+    "getcoll.c",     "getcols.c",        "getcolsb.c",         "getcoluk.c",  "getcolui.c",
+    "getcoluj.c",    "getkey.c",         "group.c",            "grparser.c",  "histo.c",
+    "iraffits.c",    "modkey.c",         "putcol.c",           "putcolb.c",   "putcold.c",
+    "putcole.c",     "putcoli.c",        "putcolj.c",          "putcolk.c",   "putcoluk.c",
+    "putcoll.c",     "putcols.c",        "putcolsb.c",         "putcolu.c",   "putcolui.c",
+    "putcoluj.c",    "putkey.c",         "region.c",           "scalnull.c",  "swapproc.c",
+    "wcssub.c",      "wcsutil.c",        "imcompress.c",       "quantize.c",  "ricecomp.c",
+    "pliocomp.c",    "fits_hcompress.c", "fits_hdecompress.c", "simplerng.c",
+    // zlib sources
+    "zcompress.c",
+    "zuncompress.c",
+    // fitsio src
+    "f77_wrap1.c",      "f77_wrap2.c",        "f77_wrap3.c", "f77_wrap4.c",
+};
+
+const CFITSIO_HEADERS = [_][]const u8{
+    "cfortran.h",
+    "drvrgsiftp.h",
+    "drvrsmem.h",
+    "eval_defs.h",
+    "eval_tab.h",
+    "f77_wrap.h",
+    "fitsio.h",
+    "fitsio2.h",
+    "fpack.h",
+    "group.h",
+    "grparser.h",
+    "longnam.h",
+    "region.h",
+    "simplerng.h",
+};
+
 pub const ROOT = _root() ++ "/";
 pub const CFITS_DIR = ROOT ++ "vendor/cfitsio-4.2.0/";
 
@@ -12,7 +49,9 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const libcfitsio = createCFITSIO(b, target);
-    libcfitsio.installHeader(CFITS_DIR ++ "fitsio.h", "fitsio.h");
+    inline for (CFITSIO_HEADERS) |header| {
+        libcfitsio.installHeader(CFITS_DIR ++ header, header);
+    }
 
     b.installArtifact(libcfitsio);
 
@@ -20,6 +59,7 @@ pub fn build(b: *std.Build) !void {
         .source_file = .{ .path = "./src/main.zig" },
         .dependencies = &.{},
     });
+
     // todo: https://github.com/ziglang/zig/pull/14731
 
     var main_tests = b.addTest(.{
@@ -48,32 +88,12 @@ pub fn createCFITSIO(b: *std.Build, target: std.zig.CrossTarget) *std.build.Comp
     //     .version = .{ .major = 0, .minor = 1 },
     // });
 
-    const sources = [_][]const u8{
-        "buffers.c",     "cfileio.c",        "checksum.c",         "drvrfile.c",  "drvrmem.c",
-        "drvrnet.c",     "drvrsmem.c",       "editcol.c",          "edithdu.c",   "eval_l.c",
-        "eval_y.c",      "eval_f.c",         "fitscore.c",         "getcol.c",    "getcolb.c",
-        "getcold.c",     "getcole.c",        "getcoli.c",          "getcolj.c",   "getcolk.c",
-        "getcoll.c",     "getcols.c",        "getcolsb.c",         "getcoluk.c",  "getcolui.c",
-        "getcoluj.c",    "getkey.c",         "group.c",            "grparser.c",  "histo.c",
-        "iraffits.c",    "modkey.c",         "putcol.c",           "putcolb.c",   "putcold.c",
-        "putcole.c",     "putcoli.c",        "putcolj.c",          "putcolk.c",   "putcoluk.c",
-        "putcoll.c",     "putcols.c",        "putcolsb.c",         "putcolu.c",   "putcolui.c",
-        "putcoluj.c",    "putkey.c",         "region.c",           "scalnull.c",  "swapproc.c",
-        "wcssub.c",      "wcsutil.c",        "imcompress.c",       "quantize.c",  "ricecomp.c",
-        "pliocomp.c",    "fits_hcompress.c", "fits_hdecompress.c", "simplerng.c",
-        // zlib sources
-        "zcompress.c",
-        "zuncompress.c",
-        // fitsio src
-        "f77_wrap1.c",      "f77_wrap2.c",        "f77_wrap3.c", "f77_wrap4.c",
-    };
-
     const cflags = switch (target.getOsTag()) {
         .macos => [2][]const u8{ "-O2", "-Dmacintosh" },
         else => [2][]const u8{ "-O2", "-Dg77Fortran" },
     };
 
-    inline for (sources) |f| {
+    inline for (CFITSIO_SOURCES) |f| {
         lib.addCSourceFile(.{ .file = .{ .path = CFITS_DIR ++ f }, .flags = &cflags });
     }
     lib.linkLibC();
